3b972865d82e3abe637f37be85761b9dfe8c900f
Pedro Zwang <pedroh.zwang@gmail.com>
2022-08-28 18:18:51 -0300

    botões de adicionar, limpar e campo de task

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,6 +5,73 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        color: Colors.black54,
+        child: Center(
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelStyle: const TextStyle(
+                              color: Colors.white,
+                            ),
+                            hintText: 'Adicionar uma tarefa',
+                            hintStyle: const TextStyle(
+                              color: Colors.white,
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.red.shade400,
+                                width: 5,
+                              ),
+                            ),
+                          ),
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.red.shade400,
+                            padding: const EdgeInsets.all(16)),
+                        child: const Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          'Você possui 0 tarefas pendentes',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.red.shade400,
+                            padding: const EdgeInsets.all(16)),
+                        child: const Text('Limpar tudo'),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
+        ),
+      ),
+    );
   }
 }
