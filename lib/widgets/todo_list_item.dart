119ef40ae2b656e9612e0ed7b004dc6fa3a32cb9
Pedro Zwang <pedroh.zwang@gmail.com>
2022-09-04 23:15:26 -0300

    removendo slidable

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:lista_tarefas/models/task.dart';
 
